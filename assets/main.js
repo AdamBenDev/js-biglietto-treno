2e4b8256f26eebf8251c90621c1b7d72755cf630
AdamBenDev <adamaya9732@gmail.com>
2023-03-08 15:17:11 +0100

    Kms and passenger age,discount for old and young

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+
+//!Richiedere all'utente di inserire il numero di chilometri e l'età del passeggero
+
+const km = parseInt(prompt("Quanti chilometri devi percorrere?"));
+const age = parseInt(prompt("Quanti anni hai?"));
+
+//!Calcola il prezzo del biglietto moltiplicando i chilometri per 0.21
+
+const ticketPrice = km * 0.21;
+
+//!Se l'età del passeggero è maggiore o uguale a 65 anni, applica uno sconto del 40% sul prezzo del biglietto
+
+if (age < 18) {
+
+    
+//! Sconto del 20% per i minorenni
+
+
+  const discount = ticketPrice * 0.2;
+  const finalPrice = ticketPrice - discount;
+  alert(`Il prezzo del tuo biglietto è di ${finalPrice.toFixed(2)} €`);
+} 
